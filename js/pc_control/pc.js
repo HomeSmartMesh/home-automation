3cd54e9e4cccddcf1e198c29e88b39953ca1109c
wassfila <wassim.filali@gmail.com>
2020-05-02 16:38:45 +0200

    fix click test

--- js/pc_control/pc.js
+++ js/pc_control/pc.js
@@ -37,16 +37,16 @@ mqtt.start()
 
 function tv_button(topic,message){
   if(message.hasOwnProperty("click")){
-    logger.verbose(`tv> ${topic} : click = ${message.click}`)
+    logger.info(`tv> ${topic} : click = ${message.click}`)
     if(message.click == "on"){
         logger.info(`tv> switching on`)
-        mqtt.publish(config.control.tv_play_sonos,"off")
-        mqtt.publish(config.control.sonos_rear,"off")
+        mqtt.publish(config.control.tv_play_sonos,"on")
+        mqtt.publish(config.control.sonos_rear,"on")
       }
     }else if(message.click == "off"){
       logger.info(`tv> switching off`)
-      mqtt.publish(config.control.tv_play_sonos,"on")
-      mqtt.publish(config.control.sonos_rear,"on")
+      mqtt.publish(config.control.tv_play_sonos,"off")
+      mqtt.publish(config.control.sonos_rear,"off")
   }
 }
 
