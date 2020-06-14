f621c5f224d512358096e2178d8460347b35971b
wassfila <wassim.filali@gmail.com>
2020-06-14 12:51:43 +0200

    helper to drop measurements

---
+++ py/influx/drop_measurements.sh
@@ -0,0 +1,7 @@
+influx -execute 'DROP MEASUREMENT "Balcony tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Bathroom tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Bedroom tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Kitchen tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Livingroom tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Office tag"' -database mqtt
+influx -execute 'DROP MEASUREMENT "Livingroom window tag"' -database mqtt
