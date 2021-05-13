c89ffe5e8672a5d2692c6e57c3e6245dfe31172c
wassfila <wassim.filali@gmail.com>
2021-05-13 14:20:29 +0200

    cleaned imports

--- py/thread_tags/thread_tags.py
+++ py/thread_tags/thread_tags.py
@@ -1,21 +1,10 @@
 #https://pypi.python.org/pypi/paho-mqtt/1.1
 import paho.mqtt.client as mqtt
-import json
-#just to get host name
 import socket 
-from time import sleep
-import time
-from math import ceil
 import logging as log
-import sys,os
 import cfg
 from mqtt import mqtt_start
-import threading
 import time
-
-def mqtt_on_message(client, userdata, msg):
-
-    return
 
 UDP_IP = "::" # = 0.0.0.0 u IPv4
 UDP_PORT = 4242
@@ -26,7 +15,7 @@ sock.bind((UDP_IP, UDP_PORT))
 # -------------------- main -------------------- 
 config = cfg.configure_log(__file__)
 #will start a separate thread for looping
-clientMQTT = mqtt_start(config,mqtt_on_message,True)
+clientMQTT = mqtt_start(config,None,True)
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -34,5 +23,5 @@ while True:
     parts = message.split("{")
     topic = parts[0]
     payload = '{'+parts[1].rstrip('\n')
-    log.debug(f"'{topic}' => {payload}")
+    log.info(f"'{topic}' => {payload}")
     clientMQTT.publish(topic,payload)
