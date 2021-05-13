92f549866482c3a65e2b738ad909ef740f6919e5
wassfila <wassim.filali@gmail.com>
2021-05-13 18:33:28 +0200

    thread tags as a service

--- py/thread_tags/thread_tags.py
+++ py/thread_tags/thread_tags.py
@@ -6,6 +6,15 @@ import cfg
 from mqtt import mqtt_start
 import time
 
+def friendly_topic(topic):
+    base_topic = topic.split('/')[0]
+    long_name =  topic.split('/')[1]
+    if(long_name in config["friendlyNames"]):
+        name = config["friendlyNames"][long_name]
+    else:
+        name = long_name
+    return base_topic+'/'+name
+
 UDP_IP = "::" # = 0.0.0.0 u IPv4
 UDP_PORT = 4242
 
@@ -21,7 +30,7 @@ while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     message = data.decode("utf-8")
     parts = message.split("{")
-    topic = parts[0]
+    topic = friendly_topic(parts[0])
     payload = '{'+parts[1].rstrip('\n')
     log.info(f"'{topic}' => {payload}")
     clientMQTT.publish(topic,payload)
