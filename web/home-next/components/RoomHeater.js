9a01e67a5c483050e9d1eb4d5108b5917eb6d13a
wass <wassim.filali@gmail.com>
2022-01-23 21:49:55 +0100

    added icons

--- web/home-next/components/RoomHeater.js
+++ web/home-next/components/RoomHeater.js
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 import {    Paper, Grid,Box, Divider,
-  Typography, Slider,LinearProgress,
-  CircularProgress} from '@mui/material';
-import {Thermostat} from '@mui/icons-material';
+            Typography, Slider,  Stack } from '@mui/material';
+
+import {Thermostat,MeetingRoom} from '@mui/icons-material';
+import BathtubIcon from '@mui/icons-material/Bathtub';
+import SoupKitchenIcon from '@mui/icons-material/SoupKitchen';
+import HomeIcon from '@mui/icons-material/Home';
+import WeekendIcon from '@mui/icons-material/Weekend';
+import LocalHotelIcon from '@mui/icons-material/LocalHotel';
+import ComputerIcon from '@mui/icons-material/Computer';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+
+import { styled } from '@mui/material/styles';
+import CircularProgress, {circularProgressClasses} from '@mui/material/CircularProgress';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 const slider_config ={
   min:5,
@@ -14,6 +25,50 @@ let slider_marks = [
   {value: 5,label: '5°',},
   {value: 27,label: '27°',},
 ];
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 5,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === 'light' ? '#d02222' : '#308fe8',
+  },
+}));
+
+function MUICircularProgress(props) {
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <CircularProgress
+        variant="determinate"
+        sx={{
+          color: (theme) =>theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        }}
+        size={40}
+        thickness={4}
+        {...props}
+        value={100}
+      />
+      <CircularProgress
+        variant="determinate"
+        sx={{
+          color: (theme) => (theme.palette.mode === 'light' ? '#d02222' : '#308fe8'),
+          animationDuration: '550ms',
+          position: 'absolute',
+          left: 0,
+          [`& .${circularProgressClasses.circle}`]: {
+            strokeLinecap: 'round',
+          },
+        }}
+        size={40}
+        thickness={4}
+        {...props}
+      />
+    </Box>
+  );
+}
 
 export default function RoomHeater({ room_id,room, onChange }) {
   const [value,setValue] = useState(12)
@@ -31,8 +86,8 @@ export default function RoomHeater({ room_id,room, onChange }) {
 
   let room_temperature = ""
   if(room.ambient.temperature != 0){
-    room_temperature = "room "+room.ambient.temperature.toFixed(1)+"°"
-    slider_marks = [{value: 5,label: '5°',},{value:room.ambient.temperature,label:"room"},{value: 27,label: '27°'}]
+    room_temperature = room.ambient.temperature.toFixed(1)+"°"
+    slider_marks = [{value: 5,label: '5°',},{value:room.ambient.temperature,label:"ambient"},{value: 27,label: '27°'}]
   }
   let metal_temperature = ""
   if("temperature" in room.metal.data){
@@ -42,16 +97,33 @@ export default function RoomHeater({ room_id,room, onChange }) {
     }
   }
   let pi_heating_demand = ("pi_heating_demand" in room.heater.data)?room.heater.data.pi_heating_demand:0
-
+  let window_open = false
+  if("eurotronic_host_flags" in room.heater.data){
+    window_open = room.heater.data.eurotronic_host_flags.window_open
+  }
+  function get_icon(){
+    if(room_id == "bathroom") return(<BathtubIcon/>)
+    else if(room_id == "livingroom") return(<WeekendIcon/>)
+    else if(room_id == "bedroom") return(<LocalHotelIcon/>)
+    else if(room_id == "office") return(<ComputerIcon/>)
+    else if(room_id == "kitchen") return(<SoupKitchenIcon/>)
+    else return(<HomeIcon/>)
+  }
 
   return(
   <div>
     <Box id="mainContent" m={1} >
       <Paper elevation={3} >
         <Box id="allCard" px={2} pt={1}>
-          <Grid id="cardHeader" container alignItems="center">
+          <Grid id="cardHeader" container alignItems="center" spacing={1}>
+            <Grid item>{get_icon()} </Grid>
             <Grid item><Typography variant="h6" >{room.name}</Typography></Grid>
-            <Grid item><Thermostat /></Grid>
+            <Grid item sx={{display: window_open?'block':'none'}}>
+              <Stack direction="row" spacing={1} alignItems="center" >
+                <Typography variant="h6" >window open</Typography>
+                <MeetingRoom />
+              </Stack>
+            </Grid>
           </Grid>
           <Box px={1}>
             <Slider value={current_heating_setpoint}
@@ -74,23 +146,37 @@ export default function RoomHeater({ room_id,room, onChange }) {
               />
           </Box>
           <Divider light />
-          <Box pt={1}>
-            <Grid container spacing={2} b={1}>
-              <Grid item xs={2}><Typography variant="h6" >{room_temperature}</Typography></Grid>
-              <Grid item xs={2}><Typography variant="h6" >{room.heater.last_seen_mn}</Typography></Grid>
-              <Grid item xs={1}><Typography variant="h6" >
-                  {(pi_heating_demand==0)?"closed":`open ${pi_heating_demand}%`} 
-                </Typography></Grid>
-              <Grid item xs={1}><CircularProgress variant="determinate" value={pi_heating_demand}color="secondary"/></Grid>
-              <Grid item xs={2}><Typography variant="h6" >{handle_temperature}</Typography></Grid>
-              <Grid item xs={2}><LinearProgress 
-                sx={{ display: (handle_metal==0)?'none':'block' }}
-                variant="determinate" 
-                value={handle_metal}  
-                color="secondary"/></Grid>
-              <Grid item xs={2}><Typography variant="h6" >{metal_temperature}</Typography></Grid>
-            </Grid>
-          </Box>
+          <Stack  direction="row"
+                  spacing={2}
+                  alignItems="center"
+                  justifyContent="flex-start"
+                  divider={<Divider orientation="vertical" flexItem />}
+                  >
+              <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={1}>
+                <Thermostat/>
+                <Typography variant="h6" >{room_temperature}</Typography>
+              </Stack>
+              <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={1}>
+                <SignalCellularAltIcon/>
+                <Typography variant="h6" >{room.heater.last_seen_mn}</Typography>
+              </Stack>
+              <Stack direction="row"
+                  alignItems="center"
+                  justifyContent="center">
+                <Typography variant="h6" >{(pi_heating_demand==0)?"closed":`open ${pi_heating_demand}%`}</Typography>
+                <Box m={1}><MUICircularProgress value={pi_heating_demand}/></Box>
+              </Stack>
+              <Stack direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center">
+                <Typography variant="h6" >{handle_temperature}</Typography>
+                <Box sx={{width:200, display: (handle_metal==0)?'none':'block'}}>
+                  <BorderLinearProgress variant="determinate" value={handle_metal}/>
+                </Box>
+                <Typography variant="h6" sx={{ display: (handle_metal==0)?'none':'block' }}>{metal_temperature}</Typography>
+              </Stack>
+          </Stack>
         </Box>
       </Paper>
     </Box>
