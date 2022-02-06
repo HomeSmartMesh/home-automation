69a9da9921446849487c8b2a2ceea5f26b2286be
wass <wassim.filali@gmail.com>
2022-02-06 16:20:50 +0100

    restored nrf_mesh

--- py/influx/check.py
+++ py/influx/check.py
@@ -6,9 +6,5 @@ db_client_config = utl.load_json("check.json")
 
 itl.create(db_client_config)
 
-#res = itl.check()
-#utl.save_json("info.json",res)
-
-pd = itl.get_pandas("Flora1")
-pd.head()
-#itl.check_measurement("shelly tv")
+res = itl.check()
+utl.save_json("nrf_mesh_info.json",res)
--- py/influx/restore.sh
+++ py/influx/restore.sh
@@ -1,1 +1,9 @@
 influxd restore -db mqtt /home/pi/share/2021-12-23_influx_backup/
+
+wass@salon:~$ sudo influxd restore -metadir /var/lib/influxdb/meta /home/wass/share/backup_lifo/
+Using metastore snapshot: /home/wass/share/backup_lifo/meta.01
+
+wass@salon:~$ sudo influxd restore -db nrf_mesh -newdb nrf_mesh -datadir /var/lib/influxdb/data /home/wass/share/backup_lifo/
+2022/02/06 15:57:13 Restoring offline from backup /home/wass/share/backup_lifo/nrf_mesh.*
+
+sudo chown -R influxdb:influxdb /var/lib/influxdb
