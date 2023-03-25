a6813f314a15d4647b9e82508ae79595302d3279
wass <wassim.filali@gmail.com>
2023-03-25 11:36:00 +0100

    add thread forming script

---
+++ matter/form.sh
@@ -0,0 +1,17 @@
+sudo ot-ctl dataset init new
+
+sudo ot-ctl dataset channel 24
+sudo ot-ctl dataset channel
+sudo ot-ctl dataset panid 0x1234
+sudo ot-ctl dataset panid
+sudo ot-ctl dataset networkname ThreadMatter
+sudo ot-ctl dataset networkname
+sudo ot-ctl dataset extpanid 1111111122222222
+sudo ot-ctl dataset networkkey 00112233445566778899aabbccddeeff
+sudo ot-ctl dataset
+
+sudo ot-ctl dataset commit active
+
+sudo ot-ctl ifconfig up
+sudo ot-ctl thread start
+sudo ot-ctl state
