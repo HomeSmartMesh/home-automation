82854a8b2c3187da2fed4f5edf1e52d80d9bed63
wass <wassim.filali@gmail.com>
2023-06-18 12:40:14 +0200

    fix assignement of replace

--- py/thread_tags/thread_tags_mqtt.py
+++ py/thread_tags/thread_tags_mqtt.py
@@ -10,7 +10,7 @@ import utils as utl
 
 def replace_friendly_names(topic,friendlyNames):
     for name,friendly_name in friendlyNames.items():
-        topic.replace(name,friendly_name)
+        topic=topic.replace(name,friendly_name)
     return topic
 
 def run():
