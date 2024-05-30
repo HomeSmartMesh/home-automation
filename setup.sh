caa6b920cc95d8b22615a5ef3be64c05cf9fdf71
wassfila <wassim.filali@gmail.com>
2024-05-30 19:09:13 +0200

    removed submodules cleanup of unused installs

--- setup.sh
+++ setup.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-# -------- install docker if not available -------- 
-## needed for docker-compose => mosquitto, influxdb, grafana
-if [ -x "$(command -v docker)" ]; then
-    echo "docker available"
-else
-    echo "Installing docker"
-    sudo bash setup_docker.sh
-    SCRIPT_REBOOT="yes"
-fi
-# -------- install docker-compose if not available -------- 
-# needed for mosquitto, influxdb, grafana
-if [ -x "$(command -v docker-compose)" ]; then
-    echo "docker-compose available"
-else
-    echo "Installing docker-compose"
-    sudo bash setup_docker-compose.sh
-    SCRIPT_REBOOT="yes"
-fi
-
 # -------- install openthread if not available -------- 
 # needed for thread_tags
 if [ -x "$(command -v ot-ctl)" ]; then
--- setup_docker-compose.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y libffi-dev libssl-dev
-sudo apt install -y python3-dev
-sudo apt-get install -y python3 python3-pip
-sudo pip3 install docker-compose
